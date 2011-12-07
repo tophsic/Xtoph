a5c5feb1ee01b58f669cc01c32d357f662af4492
Christophe Sicard <sicard.christophe@gmail.com>
2011-12-07 18:02:13 +0100

    zf create|delete propel-validator

--- library/Xtoph/Tool/Project/Propel/Schema.php
+++ library/Xtoph/Tool/Project/Propel/Schema.php
@@ -36,6 +36,8 @@ class Xtoph_Tool_Project_Propel_Schema
 {
    const XPATH_TABLE = "/database/table[@name='%s']";
    const XPATH_COLUMN = "column[@name='%s']";
+   const XPATH_VALIDATOR = "validator[@column='%s']";
+   const XPATH_VALIDATORRULE = "rule[@name='%s']";
 
    const TYPE_VARCHAR = 'VARCHAR';
    const TYPE_INTEGER = 'INTEGER';
@@ -49,10 +51,61 @@ class Xtoph_Tool_Project_Propel_Schema
    const COLUMN_ATTRIBUTE_SIZE = 'size';
    const COLUMN_ATTRIBUTE_DEFAULTVALUE = 'defaultValue';
 
+   const VALIDATOR_RULE_CLASS = 'class';
+   const VALIDATOR_RULE_MINLENGTH = 'minLength';
+   const VALIDATOR_RULE_MAXLENGTH = 'maxLength';
+   const VALIDATOR_RULE_REQUIRED = 'required';
+   const VALIDATOR_RULE_TYPE = 'type';
+
    /**
     * @var SimpleXMLElement
     */
    protected $_xml = null;
+
+   /**
+    * @todo Complete default validator rule message
+    * @param string $rule
+    * @param string $value
+    * @param string $message
+    * @param string $column
+    * @param string $table
+    * @return string 
+    */
+   public static function getValidatorRuleMessage($rule, $value, $message, $column, $table)
+   {
+      if (empty($message)) {
+         switch ($rule) {
+            case self::VALIDATOR_RULE_MINLENGTH:
+               $message = sprintf("The '%s.%s' field must be %s characters long.", $table, $column, $value);
+               break;
+            case self::VALIDATOR_RULE_MAXLENGTH:
+               $message = sprintf("The '%s.%s' field must be not longer than %s characters.", $table, $column, $value);
+               break;
+            case self::VALIDATOR_RULE_REQUIRED:
+               $message = sprintf("The '%s.%s' field is required.", $table, $column);
+               break;
+            case self::VALIDATOR_RULE_TYPE:
+               $message = sprintf("The '%s.%s' field must be an %s type.", $table, $column, $value);
+               break;
+         }
+      }
+      return $message;
+   }
+
+   public static function isValidatorRuleValid($rule, $value)
+   {
+      if (!in_array($rule,
+              array(
+              self::VALIDATOR_RULE_CLASS,
+              self::VALIDATOR_RULE_MINLENGTH,
+              self::VALIDATOR_RULE_MAXLENGTH,
+              self::VALIDATOR_RULE_REQUIRED,
+              self::VALIDATOR_RULE_TYPE
+          ))) {
+         throw new Xtoph_Tool_Project_Propel_SchemaException("Validator rule '$rule' is not valid");
+      }
+      return true;
+   }
 
    public static function isColumnAttributeValid($name, $value)
    {
@@ -204,13 +257,127 @@ class Xtoph_Tool_Project_Propel_Schema
    }
 
    /**
+    * @param string $column
+    * @param string $table
+    * @return SimpleXMLElement
+    */
+   public function addValidator($column, $table)
+   {
+      $xml = $this->getTable($table, true);
+      $validator = $xml->addChild('validator');
+      $validator['column'] = $column;
+      return $validator;
+   }
+
+   /**
+    * @param string $rule
+    * @param string $value
+    * @param string $message
+    * @param string $column
+    * @param string $table
+    * @param SimpleXMLElement $validator
+    * @return SimpleXMLElement validator node
+    */
+   public function setValidatorRule($rule, $value, $message, $column, $table, SimpleXMLElement $validator = null)
+   {
+      self::isValidatorRuleValid($rule, $value);
+      if (is_null($validator)) {
+         $validator = $this->getValidator($column, $table, true);
+      }
+      $this->removeValidatorRule($validator, $rule);
+      $message = self::getValidatorRuleMessage($rule, $value, $message, $column, $table);
+      $this->addValidatorRule($validator, $rule, $value, $message);
+      return $validator;
+   }
+
+   public function hasValidator($column, $table)
+   {
+      if (!$this->hasColumn($column, $table)) {
+         return new Xtoph_Tool_Project_Propel_SchemaException("Column '$table.$column' doesn't exists");
+      }
+      return $this->_hasNode($this->getValidator($column, $table));
+   }
+
+   /**
+    * @param string $column
+    * @param string $table
+    * @param boolean $first
+    * @return mixed array|SimpleXMLElement
+    */
+   public function getValidator($column, $table, $first = false)
+   {
+      $a = $this->_xpath(sprintf(
+              self::XPATH_TABLE . '/' . self::XPATH_VALIDATOR, $table, $column
+          ));
+      if ($first) {
+         return $a[0];
+      } else {
+         return $a;
+      }
+   }
+
+   /**
+    * @param SimpleXMLElement $validator
+    * @param string $rule
+    * @param boolean $first
+    * @return mixed array|SimpleXMLElement
+    */
+   public function getValidatorRule(SimpleXMLElement $validator, $rule,
+       $first = false)
+   {
+      $a = $this->_xpath(sprintf(
+              self::XPATH_VALIDATORRULE, $rule
+          ),
+          $validator);
+      if ($first) {
+         return $a[0];
+      } else {
+         return $a;
+      }
+   }
+
+   /**
+    * @param SimpleXMLElement $validator
+    * @param string $rule 
+    */
+   public function removeValidatorRule(SimpleXMLElement $validator, $rule)
+   {
+      $this->_removeNode($this->getValidatorRule($validator, $rule));
+   }
+   
+   /**
+    * @param string $column
+    * @param string $table
+    */
+   public function removeValidator($column, $table)
+   {
+      $this->_removeNode($this->getValidator($column, $table));
+   }
+
+   public function addValidatorRule(SimpleXMLElement $validator, $name, $value,
+       $message)
+   {
+      $rule = $validator->addChild('rule');
+      $rule['name'] = $name;
+      if (!empty($value)) {
+         $rule['value'] = $value;
+      }
+      $rule['message'] = $message;
+      return $rule;
+   }
+
+   /**
     * @param string $xpath
+    * @param SimpleXMLElement $simpleXMLElement
     * @return array
     * @throws Xtoph_Tool_Project_Propel_SchemaException if xpath gives an error
     */
-   protected function _xpath($xpath)
+   protected function _xpath($xpath, $simpleXMLElement = null)
    {
-      $a = $this->_xml->xpath((string) $xpath);
+      if (is_null($simpleXMLElement)) {
+         $simpleXMLElement = $this->_xml;
+      }
+      $a = $simpleXMLElement->xpath((string) $xpath);
       if ($a === false) {
          throw new Xtoph_Tool_Project_Propel_SchemaException('Xpath expression gives an error');
       }
--- library/Xtoph/Tool/Project/Provider/PropelValidator.php
+++ library/Xtoph/Tool/Project/Provider/PropelValidator.php
@@ -30,16 +30,162 @@ require_once 'Zend/Tool/Project/Provider/Exception.php';
  * @license    http://christophe.plom.net/license/new-bsd     New BSD License
  */
 class Xtoph_Tool_Project_Provider_PropelValidator
-    extends Xtoph_Tool_Project_Provider_Abstract
+    extends Xtoph_Tool_Project_Provider_PropelAbstract
+    implements Zend_Tool_Framework_Provider_Pretendable
 {
-   public function create()
+
+   protected $_specialties = array(
+       'Class',
+       'MinLength',
+       'MaxLength',
+       'Required',
+       'Type'
+   );
+
+   public function _createValidator($rule, $value, $message, $column, $table,
+       Xtoph_Tool_Project_Propel_Schema $schema)
    {
-      $this->_registry->getResponse()->appendContent('TODO: create action in propel-validator provider');
+      if (!$schema->hasColumn($column, $table)) {
+         throw new Zend_Tool_Project_Provider_Exception("Column '$column' does not exists in table '$table'");
+      }
+      $validator = null;
+      if (!$schema->hasValidator($column, $table)) {
+         $validator = $schema->addValidator($column, $table);
+      }
+      $validator = $schema->setValidatorRule($rule, $value, $message, $column,
+          $table, $validator);
+      return $validator;
    }
 
-   public function delete()
+   public function _deleteValidator($rule, $column, $table,
+       Xtoph_Tool_Project_Propel_Schema $schema)
    {
-      $this->_registry->getResponse()->appendContent('TODO: delete action in propel-validator provider');
+      if (!$schema->hasColumn($column, $table)) {
+         throw new Zend_Tool_Project_Provider_Exception("Column '$column' does not exists in table '$table'");
+      }
+      $validator = null;
+      if ($schema->hasValidator($column, $table)) {
+         /* @var $validator SimpleXMLElement */
+         $validator = $schema->getValidator($column, $table, true);
+         $schema->removeValidatorRule($validator, $rule);
+         if ($validator->count() == 0) {
+            $schema->removeValidator($column, $table);
+         }
+      }
+      return $validator;
+   }
+
+   public function create($rule, $value = "", $message = "", $column = "",
+       $table = "", $schema = "")
+   {
+      $this->_loadProfile(self::NO_PROFILE_THROW_EXCEPTION);
+
+      $request = $this->_registry->getRequest();
+      $response = $this->_registry->getResponse();
+
+      $schema = Xtoph_Tool_Project_Provider_Propel::getActiveSchema($this->_loadedProfile,
+              $schema);
+      $table = Xtoph_Tool_Project_Provider_Propel::getActiveTable($this->_loadedProfile,
+              $table);
+
+      $column = Xtoph_Tool_Project_Provider_Propel::getActiveColumn($this->_loadedProfile,
+              $column);
+
+      if (!is_null($schema)
+          && !is_null($table)
+          && !is_null($column)) {
+
+         if (!$this->initializeSchema($schema)) {
+            throw new Zend_Tool_Project_Provider_Exception("Schema '$schema' could not be initialized");
+         }
+
+         $validator = $this->_createValidator($rule, $value, $message, $column,
+             $table, $this->_loadedSchema);
+         Xtoph_Tool_Project_Provider_Propel::setActiveValues($this->_loadedProfile,
+             $schema, $table, $column);
+
+         if (!is_null($validator)) {
+            if ($request->isPretend()) {
+               $response->appendContent("Would create validator '$rule' for column '$table.$column'");
+            } else {
+               $response->appendContent("Creating validator '$rule' for column '$table.$column'");
+               $this->_storeSchema();
+               $this->_storeProfile();
+            }
+         }
+      } else {
+         throw new Zend_Tool_Project_Profile_Exception('Schema, table and column names should be provided');
+      }
+   }
+
+   public function createRequired()
+   {
+      $this->create(Xtoph_Tool_Project_Propel_Schema::VALIDATOR_RULE_REQUIRED);
+   }
+
+   public function createMaxLength($value)
+   {
+      $this->create(Xtoph_Tool_Project_Propel_Schema::VALIDATOR_RULE_MAXLENGTH,
+          $value);
+   }
+
+   public function createMinLength($value)
+   {
+      $this->create(Xtoph_Tool_Project_Propel_Schema::VALIDATOR_RULE_MINLENGTH,
+          $value);
+   }
+
+   public function createType($type)
+   {
+      $this->create(Xtoph_Tool_Project_Propel_Schema::VALIDATOR_RULE_TYPE, $type);
+   }
+
+   public function createClass($class)
+   {
+      $this->create(Xtoph_Tool_Project_Propel_Schema::VALIDATOR_RULE_CLASS,
+          $class);
+   }
+
+   public function delete($rule, $column = "", $table = "", $schema = "")
+   {
+      $this->_loadProfile(self::NO_PROFILE_THROW_EXCEPTION);
+
+      $request = $this->_registry->getRequest();
+      $response = $this->_registry->getResponse();
+
+      $schema = Xtoph_Tool_Project_Provider_Propel::getActiveSchema($this->_loadedProfile,
+              $schema);
+      $table = Xtoph_Tool_Project_Provider_Propel::getActiveTable($this->_loadedProfile,
+              $table);
+
+      $column = Xtoph_Tool_Project_Provider_Propel::getActiveColumn($this->_loadedProfile,
+              $column);
+
+      if (!is_null($schema)
+          && !is_null($table)
+          && !is_null($column)) {
+
+         if (!$this->initializeSchema($schema)) {
+            throw new Zend_Tool_Project_Provider_Exception("Schema '$schema' could not be initialized");
+         }
+
+         $validator = $this->_deleteValidator($rule, $column, $table,
+             $this->_loadedSchema);
+         Xtoph_Tool_Project_Provider_Propel::setActiveValues($this->_loadedProfile,
+             $schema, $table, $column);
+
+         if (!is_null($validator)) {
+            if ($request->isPretend()) {
+               $response->appendContent("Would delete validator '$rule' for column '$table.$column'");
+            } else {
+               $response->appendContent("Deleting validator '$rule' for column '$table.$column'");
+               $this->_storeSchema();
+               $this->_storeProfile();
+            }
+         }
+      } else {
+         throw new Zend_Tool_Project_Profile_Exception('Schema, table and column names should be provided');
+      }
    }
 
 }
